ed09d617c44b72814fb3c63f5071b89270447d06
afnan81 <afnanahamed379@gmail.com>
2026-03-05 17:01:06 +0530

    Add files via upload

---
+++ DisplayArr.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DisplayArr{
+
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+
+        System.out.print("enter the number of elements in array:");
+        int n = scn.nextInt();
+
+        int [] arr = new int[n];
+
+        System.out.print("enter the"+ n + "elements of the array:");
+        for(int i=0; i<n; i++){
+            arr[i]= scn.nextInt();
+        }
+
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int idx){
+      if (idx==arr.length){
+        return;
+      }
+    
+      displayArr(arr,idx + 1);
+
+        System.out.println(arr[idx]);
+
+
+    }
+    
+}
---
+++ DisplayArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DisplayArray{
+
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+
+        System.out.print("enter the number of elements in array:");
+        int n = scn.nextInt();
+
+        int [] arr = new int[n];
+
+        System.out.println("enter the"+ n + "elements of the array:");
+        for(int i=0; i<n; i++){
+            arr[i]= scn.nextInt();
+        }
+
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int idx){
+      if (idx==arr.length){
+        return;
+      }
+      System.out.println(arr[idx]);
+
+      displayArr(arr,idx + 1);
+
+    }
+    
+}
